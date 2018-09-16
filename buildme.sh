85eea40c4d6772c312230d4c684dd6fbc148250b
Yaddehi Udara Piumal De Silva <udara@chiphackers.com>
2018-09-16 03:58:58 +0000

    adding buildme

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-echo "hello world"
+DIR=`dirname "${BASH_SOURCE[0]}"`
+if [ -d "$DIR/obj" ]; then
+    rm $DIR/obj -rf
+fi
+
+mkdir $DIR/obj
+cp $DIR/examples/* $DIR/obj/ -r
+
+echo "created object folder"
 
